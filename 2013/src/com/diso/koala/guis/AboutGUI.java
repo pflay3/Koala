f0e3fe690312c4a1531d1f13691eb5af0c921758
Pablo Diaz <pablodiazvalencia@hotmail.com>
2013-07-20 16:23:50 -0500

    Se agrego pantalla de Acerca de
    En la pantalla principal se agrego boton que enlaza hacia la nueva
    pantalla

---
+++ 2013/src/com/diso/koala/guis/AboutGUI.java
@@ -0,0 +1,16 @@
+package com.diso.koala.guis;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.diso.koala.R;
+
+import java.util.Locale;
+
+public class AboutGUI extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
--- 2013/src/com/diso/koala/guis/Home.java
+++ 2013/src/com/diso/koala/guis/Home.java
@@ -25,6 +25,7 @@ public class Home extends Activity {
         StartActivity((Button)findViewById(R.id.btnProductList), ProductListGUI.class);
         StartActivity((Button)findViewById(R.id.btnCustomerList), CustomerListGUI.class);
         StartActivity((Button)findViewById(R.id.btnSaleList), SaleListGUI.class);
+        StartActivity((Button)findViewById(R.id.btnAbout), AboutGUI.class);
     }
 
     void StartActivity(Button b, final Class<?> cls){
